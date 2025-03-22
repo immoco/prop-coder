276175400004236bc9b1592d0c4a2d650b75abbb
Murugavel14 <murugavelkumar14@gmail.com>
2025-03-22 13:19:28 +0530

    Create favicon.js

---
+++ favicon.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" href="disabled-person.png" />
+        <title>React App</title>
+      </head>
+      <body>
+        <h1>Welcome to My React App</h1>
+      </body>
+    </>
+  );
+};
+
+export default App;
